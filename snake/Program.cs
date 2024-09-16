a42ef0531b46a63e15982959b0fca7a9885974bd
PivoWarZ <academik87@gmail.com>
2024-09-16 19:28:48 +0300

    lesson 4
    Инкапсуляция и списки List<>

--- snake/Program.cs
+++ snake/Program.cs
@@ -5,12 +5,36 @@ using System.Runtime.CompilerServices;
 
 Point p1 = new Point(3, 5, '*');
 
-    p1.Draw();
+   // p1.Draw();
 
 
 Point p2 = new Point(1, 2, '#');
 
-    p2.Draw();
+   // p2.Draw();
 
+//Console.ReadLine();
+
+List<int> inList = new List<int>();
+
+    inList.Add(1);
+    inList.Add(2);
+    inList.Add(3);
+
+    foreach (int i in inList)
+    {
+
+        Console.WriteLine(i);
+
+    }
+
+List<Point> list = new List<Point>();
+list.Add(p1);
+list.Add(p2);
+
+foreach (Point i in list)
+    {
+
+        i.Draw();
+
+    }
 Console.ReadLine();
-
--- snake/obj/Debug/net8.0/snake.AssemblyInfo.cs
+++ snake/obj/Debug/net8.0/snake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1140e93ec6f02b63e899e88b8bbb50471ae73539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a405f68c60ec4e6aabff32a115c380a136bce6cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
